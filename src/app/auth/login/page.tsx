64e0f3f157d8f3733f1dd1cf8fbd5feded31a71b
skygate-creator <programing@skygatesa.com>
2026-07-12 17:14:51 +0300

    edit in ui in login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -19,14 +19,14 @@ const login = () => {
 
   return (
     <section className="w-screen h-screen flex items-center justify-center px-5">
-      <div className="grid sm:grid-cols-2 gap-2 bg-white shadow-lg">
-        <div className="text flex flex-col gap-3 p-5">
-          <div className="flex flex-col gap-1">
+      <div className="grid sm:grid-cols-2 gap-2 bg-white rounded-2xl shadow-lg max-w-[1100px] mx-auto">
+        <div className="text flex flex-col gap-3 p-5 ">
+          <div className="flex flex-col gap-1 ">
             <h1 className="text-40 font-bold text-secondary-950">
               تسجيل الدخول
             </h1>
             <p className="text-18 text-neutral-700">
-              قم بتسجيل الدخول للوصول إلى لوحة التحكم.
+              قم بتسجيل الدخول للوصول إلى لوحة التحكم
             </p>
           </div>
           <form onSubmit={formik.handleSubmit} className="flex flex-col gap-3">
@@ -58,14 +58,14 @@ const login = () => {
           </form>
         </div>
         <div className="relative hidden sm:block">
-          <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-primary-500 to-primary-900 opacity-50"></span>
+          <span className="absolute rounded-e-2xl top-0 left-0 w-full h-full bg-gradient-to-r from-primary-500 to-primary-900 opacity-50"></span>
           <Image
             src="/login_img.webp"
             alt="login"
             width={300}
             height={500}
             priority
-            className="object-cover w-full h-full"
+            className="object-cover w-full h-full rounded-2xl"
           />
         </div>
       </div>
